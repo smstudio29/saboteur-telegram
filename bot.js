45e9e03b50818fb6e0d26e0c32422f92e71fb25c
smstudio29 <smstudio29@gmail.com>
2026-03-24 16:46:45 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,11 @@
+const { Telegraf } = require("telegraf")
 
+const bot = new Telegraf(process.env.BOT_TOKEN)
+
+bot.start((ctx) => {
+    ctx.reply("⛏ Добро пожаловать в Saboteur!")
+})
+
+bot.launch()
+
+console.log("Bot started")
